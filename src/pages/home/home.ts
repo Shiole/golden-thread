6fde2dfa5b9d40cf286655ea39356d5904ec1970
Shiole <kitty98low@gmail.com>
2018-05-29 14:35:53 +0200

    Add all home/login/registration/profile pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegistrationPage } from '../registration/registration';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,15 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  navigateToLogin() {
+    this.navCtrl.push(LoginPage);
+  }
+
+  navigateToRegistration() {
+    this.navCtrl.setRoot(HomePage);
+    this.navCtrl.push(RegistrationPage);
   }
 
 }
